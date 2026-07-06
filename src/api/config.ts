0fc5994ac83a664f60f35144f49590bcbd0fcd7c
faghi35 <ton_email_github@example.com>
2026-07-06 14:41:57 +0200

    Initial commit React app

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,13 +2,14 @@
 const IS_PROD = import.meta.env.PROD;
 
 // URL de base pour les appels API
-export const API_BASE_URL = IS_PROD 
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || (IS_PROD 
     ? 'https://malaika-tv.forka.org/api' 
-    : '/api'; // Proxy Vite en développement
+    : '/api'); // Proxy Vite en développement ou via Vercel Rewrite
 
 // URL de base pour les médias (images locales)
-export const MEDIA_BASE_URL = IS_PROD
+export const MEDIA_BASE_URL = import.meta.env.VITE_MEDIA_BASE_URL || (IS_PROD
     ? 'https://malaika-tv.forka.org' 
-    : `http://${window.location.hostname}/malaika-tv-api`; // En dev
+    : `http://${window.location.hostname}/malaika-tv-api`); // En dev
 
 export default API_BASE_URL;
+
